eb16b43616d2c70ee92ace8c302f5115b1a322cf
unknown <100526486@UOSL14-PK0DCV2.oncampus.local>
2015-04-11 14:12:38 -0400

    Major code changes

--- CSCI2020u/a5/Client.java
+++ CSCI2020u/a5/Client.java
@@ -9,65 +9,77 @@ public class Client{
 	OutputStream output;
 	DataInputStream dataIn;
 	DataOutputStream dataOut;
+	String login;
 
 	static public void main(String[] args) throws Exception{
-		Client c = new Client(args[0],Integer.parseInt(args[1]));
+		Client c = new Client(args[0],Integer.parseInt(args[1]),args[2]);
 		c.connect();
 	}
 
-	public Client(String hostname, int socket) throws Exception{
+	public Client(String hostname, int socket, String name) throws Exception{
 		commsocket = new Socket(hostname,socket);
 		input = commsocket.getInputStream();
 		output = commsocket.getOutputStream();
 		dataIn = new DataInputStream(input);
 		dataOut = new DataOutputStream(output);
+		login = name;
 	}
 
 	public void connect() throws Exception{
-		Thread interact = new Interact(this.dataOut);
+		Thread interact = new Interact(this.dataOut,this.dataIn,login);
 		interact.start();
-		iteract.join();
+		interact.join();
 	}
 
 	static class Interact extends Thread{
 		DataOutputStream dataOut;
-		String match = "(?<command>[A-Za-z]+)[\\s]+(?<other>(.+))";
+		DataInputStream dataIn;
+		String login;
+		String match = "(?<command>[A-Za-z]+)[\\s]?+(?<other>(.+))?[\\s]?+(?<message>(.+))?";
 
-		public Interact(DataOutputStream dataOut){
+		public Interact(DataOutputStream dataOut, DataInputStream dataIn, String login){
 			this.dataOut = dataOut;
-			Scanner scanner = new Scanner(System.in);
-			String line;
+			this.dataIn = dataIn;
+			this.login = login;
 			System.out.println("Ready");
-			Pattern pattern = Pattern.compile(match);
-			Matcher matcher;
 		}
 		public void run(){
+			Scanner scanner = new Scanner(System.in);
+			Pattern pattern = Pattern.compile(match);
+			Matcher matcher;
+			String line, in;
+			try{
+				dataOut.writeUTF("login " + login);
+			}catch(Exception e){
+				System.err.println("Login error");
+			}
 			while(true){
 				try{
-					System.out.println("Command");
+
 					line = scanner.nextLine();
 					matcher = pattern.matcher(line);
-					System.out.println(line);
 					if(matcher.find()==true){
-						System.out.println(matcher.group(0));
 						if(matcher.group("command").compareToIgnoreCase("fetch")==0){
-							String in = dataIn.readUTF();
+							dataOut.writeUTF("fetch "+login);
+							in = dataIn.readUTF();
 							if(in == ""){
 								System.out.println("No new messages");
 							}
 							else{
 								System.out.println(in);
-							};
-						}else if(matcher.group("command").compareToIgnoreCase("send")==0){
-							dataOut.writeUTF(matcher.group("other"));
-							dataOut.flush();
+							}
+						}else if(matcher.group("command").compareToIgnoreCase("login")==0){
+							dataOut.writeUTF(line);
+						}
+						else if(matcher.group("command").compareToIgnoreCase("send")==0){
+							dataOut.writeUTF(line);
 						}
 					}else{
 						System.out.println("Invalid command");
 					}
-				} catch(Exception e){
+				}catch(Exception e){
 					break;
-				}
+				} 
 			}
 		}
 	}
--- CSCI2020u/a5/Server.java
+++ CSCI2020u/a5/Server.java
@@ -1,29 +1,90 @@
 import java.io.*;
 import java.net.*;
+import java.util.*;
+import java.util.regex.*;
 
 public class Server{
+	ServerSocket serverSocket;
+	public Server(int port) throws Exception{
+		serverSocket = new ServerSocket(port);
+	}
 	static public void main(String[] args) throws Exception{
 		int port = Integer.parseInt(args[0]);
-		ServerSocket serversocket = new ServerSocket(port);
-		Socket commsocket = serversocket.accept();
-		InputStream input = commsocket.getInputStream();
-		OutputStream output = commsocket.getOutputStream();
-		DataInputStream dataIn = new DataInputStream(input);
-		DataOutputStream dataOut = new DataOutputStream(output);
-		Map<String,String[]> storage = new TreeMap<String,String[]>();
-		String in = "";
+		Server server = new Server(port);
+		server.serverRun();
+	}
+
+	public void serverRun() throws Exception{
+		System.out.println("Server is running");
 		while(true){
-			in = dataIn.readUTF();
-			if(in.compareToIgnoreCase("fetch")){
-				dataOut.writeUTF(in);
-			}
-		}
+			Socket socket = serverSocket.accept();
+			InputStream input = socket.getInputStream();
+			OutputStream output = socket.getOutputStream();
+			DataInputStream dataIn = new DataInputStream(input);
+			DataOutputStream dataOut = new DataOutputStream(output);
+			(new Recieve(dataIn)).start();
 
-		static class fetch{
-			public fetch(){
+		}
+	}
+	static class Fetch extends Thread{
+		DataOutputStream dataOut;
+		public Fetch(DataOutputStream dataOut){
+			this.dataOut = dataOut;
+		}
+		public void run(){
+			try{
 				dataOut.writeUTF("Server");
+			}catch(Exception e){
+				System.err.println("Fetch error");
 			}
 		}
+	}
 
+	static class Recieve extends Thread{
+		DataInputStream dataIn;
+		Map<String,ArrayList<String>> messages = new HashMap<String,ArrayList<String>>();
+		String match = "(?<command>[A-Za-z]+)[\\s]?+(?<other>(.+))?[\\s]?+(?<message>(.+))?";
+		public Recieve(DataInputStream dataIn){
+			System.out.println("A user has connected");
+			this.dataIn = dataIn;
+		}
+		public void run(){
+			String in;
+			ArrayList<String> tempMess;
+			Scanner scanner = new Scanner(System.in);
+			Pattern pattern = Pattern.compile(match);
+			Matcher matcher;
+			while(true){
+				try{
+					in = dataIn.readUTF();
+					matcher = pattern.matcher(in);
+					if(matcher.group("command").compareToIgnoreCase("login")==0){
+						synchronized(this.messages){
+						if(!messages.containsKey(matcher.group("other"))){
+								messages.put(matcher.group("other"),(new ArrayList<String>()));
+							}
+						}
+					}else if(matcher.group("command").compareToIgnoreCase("send")==0){
+						synchronized(this.messages){
+							if(messages.containsKey(matcher.group("message"))){
+								tempMess = messages.get(matcher.group("other"));
+								tempMess.add(matcher.group("other"));
+								messages.put(matcher.group("other"),tempMess);
+							}
+						}
+					}else if(matcher.group("command").compareToIgnoreCase("fetch")==0){
+						synchronized(this.messages){
+							if(message.containsKey(matcher.group("other"))){
+								tempMess = message.get(matcher.group("other"));
+								for(String temp:tempMess){
+									dataOut.writeUTF(temp);
+								}
+							}
+					}
+				}catch(Exception e){
+					break;
+				}
+			}
+		}
 	}
 }
